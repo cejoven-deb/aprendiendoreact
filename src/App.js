b41883925007ce829819212c287f72497c1bd580
cejoven-deb <carlosvivax77@hotmail.com>
2022-06-25 20:38:32 -0500

    cambio1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+function Holamundo(nombre){
+  return nombre;
+  var presentacion = <h2>Hola, me llamo {nombre}</h2>
+  return presentacion; /* Para que me devuelva los valores de la variable presentación*/
+
+
+}
 
 function App() {
+  var nombre ="Carlos Eduardo Joven Garcia";
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Este es el primer curso de react desde cero
         </p>
+        {Holamundo((nombre))}
+        {alert("Hola Mundo con React")}
         <a
+       
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
